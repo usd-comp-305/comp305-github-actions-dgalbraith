486b7eeca75d16495a59cced3c30b01a4f514914
devongalb <dgalbraith@sandiego.edu>
2026-04-25 12:25:12 -0700

    fix: added the newline at the end of the test file to correct build.gradle error

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -1,9 +1,11 @@
 package edu.sandiego.comp305;
 
-public class StringUtils {
+public final class StringUtils {
 
-    public static String reverseString(String string) {
-        return new StringBuilder(string).reverse().toString();
+    private StringUtils() {
     }
 
+    public static String reverseString(final String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
 }
